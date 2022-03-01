c342c5071525892268a1dfdb32958ac01a55292c
LeonardoPereiraa <56571480+LeonardoPereiraa@users.noreply.github.com>
2022-03-01 11:34:54 -0300

    Update doacao.sol

--- doacao.sol
+++ doacao.sol
@@ -1,70 +1,71 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.6.0;
+
+interface IERC20 {
+
+  function totalSupply() external view returns (uint256);
+  function balanceOf(address account) external view returns (uint256);
+  function allowance(address owner, address spender) external view returns (uint256);
+
+  function transfer(address recipient, uint256 amount) external returns (bool);
+  function approve(address spender, uint256 amount) external returns (bool);
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 
 
-contract hospital {
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
-    struct  medicalInstitution{
-        string nome;
-        string email;
-        uint256 identificador;
-        bool funcionando ;
-        mapping(address => bool) activeDoctors;
-        address[] listDoctor; 
-    }
-    mapping (address => medicalInstitution)   addressToMedicalInstitution;
+contract Doacao {
+  IERC20 public tokenMPE1;
+  IERC20 public tokenMPE2;
+  address public _owner;
+
+  constructor(address owner,IERC20 MPE1,IERC20 MPE2) public {
+    _owner = owner;
+    tokenMPE1 = MPE1;
+    tokenMPE2 = MPE2;
+  }
+
+  function donationMPE1(address  recepient, uint256 value, uint256 cents) public{
+    require(msg.sender== _owner);
+    uint256 mpe1 = formatValue(value,cents,2 );
+    tokenMPE1.transfer(recepient,mpe1);
     
-    function getMedicalInstitution(address  Institution ) public view returns(string memory, string memory, uint256,bool){
-        return( addressToMedicalInstitution[Institution].nome,
-        addressToMedicalInstitution[Institution].email,
-        addressToMedicalInstitution[Institution].identificador,
-        addressToMedicalInstitution[Institution].funcionando) ;
-    }
-    function getMedicalList(address  Institution)public  view returns(address[] memory){
-        return addressToMedicalInstitution[Institution].listDoctor;
-    }
-    function checkDoctor(address Institution,address doctor) public view returns(bool){
-        return addressToMedicalInstitution[Institution].activeDoctors[doctor];
-    }
-    function addDoctor(address doctor) external {
-        require(InstitutionIsWorking(msg.sender));
-        addressToMedicalInstitution[doctor].listDoctor.push(doctor);
-    }
-    function removeDoctor(address doctor) public {
-        require(InstitutionIsWorking(msg.sender));
-        uint256 indexToremove = getIndexListDoctor(doctor);
-        uint256 indexLastElement = addressToMedicalInstitution[msg.sender].listDoctor.length -1;
-        addressToMedicalInstitution[msg.sender].listDoctor[indexToremove] = addressToMedicalInstitution[msg.sender].listDoctor[indexLastElement];
-        addressToMedicalInstitution[msg.sender].listDoctor.pop();
-        addressToMedicalInstitution[msg.sender].activeDoctors[doctor]=false;
-        
-    }
-    function getIndexListDoctor(address doctor) internal view  returns(uint256){
-        require(InstitutionIsWorking(msg.sender));
-        for (uint index = 0; index < addressToMedicalInstitution[msg.sender].listDoctor.length - 1; index++) {
-            if(doctor == addressToMedicalInstitution[msg.sender].listDoctor[index]){
-                return index;
-            }
+  }
+  function donationMPE2(address  recepient, uint256 value, uint256 cents) public{
+    require(msg.sender== _owner);
+    uint256 mpe2 = formatValue(value,cents,2 );
+    tokenMPE2.transfer(recepient,mpe2);
+    
+  }
+  fallback() external payable {
+      
+  }
+  function formatValue(uint256 _value,uint256 _cents, uint256 decimalNumber) internal returns(uint256){
+    uint256 value = _value * (10**decimalNumber);
+    uint256 cents =  _cents * (10** (decimalNumber-2));
+    return value + cents;
 
-        }
-        require(false);
-        return 0;
-        
-    }
-    function InstitutionIsWorking(address istitution) internal view returns (bool) {
-        return addressToMedicalInstitution[istitution].funcionando;
-    }
-
+  }
+   
+  function donationETH(address payable recepient, uint256 value,uint256 cents) external{
+    require(msg.sender== _owner);
+    uint256 eth = formatValue(value,cents,18);
+    recepient.transfer( eth );
+    
+  }
+  
+  
+  function getBalanceEth() external view returns(uint256){
+    return address(this).balance;
+  }
+  function getBalanceMpe1() external view returns(uint256){
+    address conta = address(this);
+    return tokenMPE1.balanceOf(conta);
+  }
+  function getBalanceMpe2() external view returns(uint256){
+    address conta = address(this);
+    return tokenMPE2.balanceOf(conta);
+  }
 }
-
-
-contract doctorContract{
-
-    struct doctor{
-        string name;
-        uint NumberIdentifier;
-        address[] activeHospital;
-    }
-    mapping(address => doctor) addressToDoctor;
-}
-
